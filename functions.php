ccac1186062f6b5dc4329527064aa658e3c3551a
Andrey <zippy1336@gmail.com>
2026-02-13 15:57:10 +0300

    добавил базовую вёрстку, styles.css, вынес php логику в отдельный файл

---
+++ functions.php
@@ -0,0 +1,28 @@
+<!-- <?php
+// $arr = [
+//     "photo_1" => [
+//         "category_name",
+//         "img_path"
+//     ],
+//     "photo_2" =>
+//     [
+//         "fruits",
+//         "/photos/banana.jpg"
+//     ]
+
+// ];
+// foreach($arr as $key => $value)
+// {
+//     $category = $value[0];
+//     $path = $value[1];
+//     echo "<br>";
+//     echo $category;
+//     echo "\n";
+//     echo "<br>";
+//     echo "<img src=\"$path\"width=\"440\">";
+//     echo "\n";
+// }
+// function sayHello(){
+//     return "hello world";
+// }
+?> -->
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php 
+include "functions.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styles.css">
+    <title>Document</title>
+</head>
+<body>
+<div class = "wrap-parent">
+    <h1>The product galery</h1>
+    <div class = "wrap-child">
+        <p>awesome product prepared with creative ideas and great design</p>
+        <nav>
+              <a href="/category/all">All</a>
+              <a href="/category/print">Print</a>
+              <a href="/category/webdesign">Web-desing</a>
+              <a href="/category/logo">Logo</a>
+              <a href="/category/motion">Motion</a>
+        </nav>
+        <div class="gallery">
+            <div class="photo">здесь будет фото </div>
+            <div class="photo">здесь будет фото </div>
+            <div class="photo">здесь будет фото </div>
+            <div class="photo">здесь будет фото </div>
+            <div class="photo">здесь будет фото </div>
+            <div class="photo">здесь будет фото </div>
+
+        </div>
+    </div>  
+
+</div>
+
+
+</body>
+</html>
